30036ea90e36d67be0a1ad6d0bd1090b097ac45d
Stephanie Hickman <stephick@gmail.com>
2024-01-28 18:04:46 -0600

    progress so far

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,9 +1,13 @@
 console.log('client.js is sourced!');
 
+onReady();
+
 function onReady(){
     getCalculations ();  //We want to show the past calculations as soon as the site loads
 
 }
+
+let operator;
 
 function getCalculations (){
     // get calculations from the server using Axios
@@ -13,7 +17,7 @@ function getCalculations (){
     })
 
     .then(function (response) { 
-        console.log("Request to GET /quotes succeeded with status:", response.status);  
+        console.log("Request to GET /calculations succeeded with status:", response.status);  
         console.log("Response payload:", response.data); // <- The content of the response (what the server sent) is in the .data field of the response object
         // Always console log it, so you can see what kind of data you are working with
 
@@ -30,21 +34,76 @@ function getCalculations (){
     });
 }
 
-function renderToDOM (calcuations){
+function renderToDOM (calculations){
         // Select the element from the DOM, that we want to use as a container for our data
-        let outputElement = document.getElementById('output');
+        let outputElement = document.getElementById('resultHistory');
         // Empty the output element, so we don't keep the old results.
         // We want to start fresh each time we add the list, since we always add the whole list.
         //outputElement.innerHTML = '';
     
         // Add the quotes to the DOM.
-        for (let calculation of calcuations) {
+        for (let calculation of calculations) {
             // Create a <li> for each calculation and append it to outputElement
             outputElement.innerHTML += `
                 <li>
-                  ${calcuations.numOne} ${calcuations.operator} ${calcuations.numTwo} '=' ${calcuations.result}
+                  ${calculation.numOne} ${calculation.operator} ${calculation.numTwo} = ${calculation.result}
                 </li>
               `;
         }
     }
 
+function onAddClick(event){
+    event.preventDefault();
+    operator = "+";
+}   
+
+
+function onMinusClick(event){
+    event.preventDefault();
+    operator = "-";
+}
+    
+function onMultiplicationClick(event){
+    event.preventDefault();
+    operator = "*";
+}
+
+function onDivisionClick(event){
+    event.preventDefault();
+    operator = "/";
+}
+    
+
+function onEqualClick(event){
+    //to stop the page from refreshing:
+    event.preventDefault();
+
+    //Grab the input from the DOM
+    let newFirstNumber = document.getElementById('first_number').value;
+    let newSecondNumber = document.getElementById('second_number').value;
+    let newOperator = operator;
+    
+    axios({
+        method: 'POST',
+        url: '/calculations',
+        data: {
+            calculationToAdd: {
+            numOne: newFirstNumber,
+            numTwo: newSecondNumber,
+            operator: newOperator
+            }
+        }
+    })
+
+        .then(function (response) {
+            console.log ("Request to POST /calculations succeeded with status:", response.status);
+
+            
+            getCalculations()
+        })
+        .catch(function (error){
+            alert('Request to POST /calculations failed.');
+            console.error('Request to POST /calculations failed:', error);
+        });
+
+}
--- server/server.js
+++ server/server.js
@@ -14,9 +14,11 @@ let calculations = [
     operator: '+',
     result: 8
   }
+];
 
-]
+let latestEquation;
 
+let latestResult;
 
 // Here's a wonderful place to make some routes:
 
@@ -24,10 +26,50 @@ let calculations = [
 
 app.get('/calculations', function (req, res){
   console.log ('Request for GET /calculations was made');
+  
+  //set status
+  res.status(200);
+  //send
   res.send(calculations)
 });
 
 // POST /calculations
+
+
+
+app.post('/calculations', function (req, res) {
+  // The data, (the object we put in the body field of our POST in axios) is sent to us.
+  // It shows up in `req.body`
+  // Note that without express.json() or bodyParser setup above, req.body will be empty or undefined!
+
+  // let newNumbersToCalculate = req.body;
+  // console.log(`We got some POST data to /calculations:`, newNumbersToCalculate);
+
+  // Grab the info we want from the request body
+  // This is different every time, so that's why we like to console log the data, so we can see what we are working with.
+  let calculationToAdd = req.body.calculationToAdd;
+  console.log(`Adding new calculation: `, calculationToAdd);
+
+  function newCalculationResult(){
+    if (calculationToAdd.operator === "+"){
+      latestResult = calculationToAdd.numOne + calculationToAdd.numTwo
+    } else if (calculationToAdd.operator === "-"){
+      latestResult = calculationToAdd.numOne - calculationToAdd.numTwo
+    } else if (calculationToAdd.operator === "*"){
+      latestResult = calculationToAdd.numOne * calculationToAdd.numTwo
+    } else if (calculationToAdd.operator === "/"){
+      latestResult = calculationToAdd.numOne / calculationToAdd.numTwo
+    } 
+  }
+newCalculationResult();
+console.log ('The latest result is: ', latestResult)
+
+  // // Push the new calculation into our array
+  // calculations.push(calculationToAdd);
+
+  // Send back a status code of 201
+  res.sendStatus(201);
+});
 
 
 // PLEASE DO NOT MODIFY ANY CODE BELOW THESE BEARS:
